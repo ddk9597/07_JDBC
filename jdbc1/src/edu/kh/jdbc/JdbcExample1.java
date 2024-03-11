937eec12b789a869d3dcaf3c864a1a9c374c0113
ddk9595 <027620@naver.com>
2024-03-11 14:41:26 +0900

    gitignore로 깃에 정보 감추기

--- jdbc1/src/edu/kh/jdbc/JdbcExample1.java
+++ jdbc1/src/edu/kh/jdbc/JdbcExample1.java
@@ -7,7 +7,7 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
-public class JDBCExample {
+public class JdbcExample1 {
 	
 	public static void main(String[] args) {
 		
